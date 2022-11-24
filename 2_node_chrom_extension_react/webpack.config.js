8234347bc82c653da340710bc1b85edd834b1339
MrParkerZ7 <neverzero9@gmail.com>
2022-11-25 00:03:39 +0700

    [add] lesson v2

---
+++ 2_node_chrom_extension_react/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    popup: "./src/popup.jsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 ### Tutorials
 
 - 1 : https://youtu.be/zHIryKuhYA4
+   - basic with manifest v2
    - Remark: manifest v2 will deprecate soon in 2023
-- 2 :
+- 2 : https://youtu.be/8OCEfOKzpAw
 - 3 : 
