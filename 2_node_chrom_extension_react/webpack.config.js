6e4246e06ca2cc38eb1ca70f194a57c9a7e99800
MrParkerZ7 <neverzero9@gmail.com>
2022-12-13 02:55:12 +0700

    [update]

--- 2_node_chrom_extension_react/webpack.config.js
+++ 2_node_chrom_extension_react/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   entry: {
     popup: "./src/popup.jsx",
   },
